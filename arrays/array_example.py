d3128a804f06e356e793e8c13faa4ce402eaa4b7
Yagmur Atmaca <yatmaca022@gmail.com>
2026-05-14 21:25:55 +0300

    Add basic array examples

--- arrays/array_example.py
+++ arrays/array_example.py
@@ -1,3 +1,24 @@
 # Array Example
 numbers = [1, 2, 3, 4, 5]
 print("Array:", numbers)
+
+print(numbers[1])
+print(numbers[2])
+print(numbers[3]) 
+print(numbers[4])
+#indeks sıfırdan başlıyor çünkü 
+# adres= başlangıç_adres + index * veri_boyutu
+
+#DİZİYİ GEZME 
+for number in numbers:
+    print(number)
+
+#Dizideki tüm elemanları tek tek ziyaret etmek.
+
+for i in range(len(numbers)):
+    print(f"Index: {i} Value: {numbers[i]}")
+
+#Burada: 
+#len(numbers) -> eleman sayısı 
+#range() -> sayı üretir 
+#numbers[i] -> index erişimi
