2ca98ab8e902e0426152f3efcf8aa41f33579be7
Yagmur Atmaca <yatmaca022@gmail.com>
2026-05-14 22:16:31 +0300

    Add linear search example

---
+++ arrays/linear_search.py
@@ -0,0 +1,26 @@
+#linear search example 
+numbers = [10 , 20 , 30, 40 , 50]
+target = 30 
+
+found = False 
+for i in range(len(numbers)):
+
+    if numbers[i] == target:
+        print(f"Element fount at index{i}")
+        found = True
+        break
+
+if not found:
+    print("Element not found")
+
+target = 30 #bulmak istediğimiz değer
+
+found = False 
+
+range(len(numbers))
+if numbers[i] == target:
+    print(f"Element found at index {i}")
+
+    break
+if not found: #eğer bulunamadıysa
+    
